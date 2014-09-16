ec9c91b58b7951e84c71ab705b6c76d89d33f3c3
Henri Kauppinen <henri.kauppinen@orum.fi>
2014-09-16 17:11:03 +0300

    Refactoring, only one repository directory and rsync to deployment directories.

--- git-deploy-branches.sh
+++ git-deploy-branches.sh
@@ -1,125 +1,75 @@
-#!/bin/sh
+#!/bin/bash
 # Script for deploying all repository branches
 # Author: Henri Kauppinen
 
-basedir=$(dirname $0)
+basedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-conffilename=conf.cfg
+# read config
+. conf.cfg
 
-if [ -e $basedir/$conffilename ]; then
-    . $basedir/$conffilename
-    link_config=$rootdir/app_files/links.cfg
-else
-    $1=""
-fi
+indexfile=$deployment_dir/index.html
 
-pruning() {
-    cd $rootdir
-
-    if [ ! -d $rootdir/deleted ]; then
-        mkdir $rootdir/deleted
+init_local_repository(){
+    if [ ! -d $local_repository_dir ]; then
+        mkdir $local_repository_dir
     fi
 
-        (cd master;git remote prune origin) | sed -e '/Pruning origin/,+d' -e '/URL\:/,+d' -e 's/ \* \[pruned\] origin\///' | while read branchname ;
-        do
-            # @todo safe mode, just move the branch folders to deleted directory for now
-            if [ -d $branchname ];then
-                    echo "Pruning $branchname"
-                    mv $branchname deleted/$branchname
-            fi
-        done
+    (cd $local_repository_dir; git clone $remote_repository .)
 }
 
-create_links(){
-    if [ -e $link_config ]; then
-        cat $rootdir/shared/links.cfg | while read link ;
-        do
-            echo "Creating links for $1"
-            IFS=' ' read -a paths <<< "${link}"
-            if [ ! -L $rootdir/$1/${paths[1]} ]; then
-                ln -s $rootdir/shared/${paths[0]} $rootdir/$1/${paths[1]}
-            fi
-        done
-    fi
+link_files(){
+    echo "Copying files to $deployment_dir/$1"
+    cp -R $basedir/app_files/* $deployment_dir/$1/
 }
 
-update(){
-    cd $rootdir
+deploy_branch() {
+    echo "Deploying $1"
 
-        (cd master; git pull origin master -q;git branch -r) | sed -e '/master/,+d' -e 's/origin\///' | while read branchname ;
-        do
-            if [ -d $branchname ]; then
-            echo "Checking updates for $branchname"
-                        (cd $branchname;git pull -q)
-                else
-            echo "Creating $branchname"
-                        git clone $remote -b $branchname --depth 1 $branchname
-                fi
+    if [ ! -d $deployment_dir ]; then
+        mkdir $deployment_dir
+    fi
 
-        create_links $branchname
+    if [ ! -d $deployment_dir/$1 ]; then
+        mkdir $deployment_dir/$1
+    fi
 
-        done
+    rsync -ap -v --stats --exclude ".git" --exclude ".gitignore" --delete --delete-excluded $local_repository_dir/ $deployment_dir/$1
 
-    # check links for master as well
-    create_links master
+    git log -p -1 > $deployment_dir/$1/gitlog.txt
+
 }
 
-installation(){
-
-    if [ ! $remote == "" ]; then
-        remote_sug=$remote
-    else
-        remote_sug="git@github.com:username/repository.git"
-    fi
-    if [ ! $rootdir == "" ]; then
-        rootdir_sug=$rootdir
-    else
-        rootdir_sug="/var/www/html/repository"
-    fi
-
-    echo "Enter full repository url (example: $remote_sug):"
-        read -r remote -e -i
-
-        echo "Enter directory where you want to deploy (ie: $rootdir_sug)"
-        read -r rootdir
-
-        # create install directory
-    # @todo instead of checking for directory existence, check if contains files or directories
-        if [ ! -d $rootdir ]; then
-        echo "Creating configuration..."
-            mkdir $rootdir
-        mkdir $rootdir/shared
-        touch $rootdir/shared/links.cfg
-        echo "remote=$remote" >> $conffilename
-        echo "rootdir=$rootdir" >> $conffilename
-
-        # init repositories
-        echo "Cloning master..."
-        (cd $rootdir;git clone $remote -b master --depth 1 master)
-        echo "Cloning branches..."
-        update
-        echo "Installation complete!"
-        else
-        echo "Directory $rootdir exists! Not doing anything just to be safe."
-    fi
+make_indexfile_row(){
+    echo "<a href='$1/'>$1</a><br />" >> $indexfile
+    echo "<pre>" >> $indexfile
+    git log -p -1 >> $indexfile
+    echo "</pre><hr/>" >> $indexfile
 }
 
 # main
-if [ "$1" == "install" ]; then
-    installation
-elif [ "$1" == "update" ]; then
-    update
-    pruning
-elif [ "$1" == "fetch" ]; then
-    update
-elif [ "$1" == "prune" ]; then
-    pruning
-else
-    echo "Usage: git-deploy-branches.sh [install|update|fetch|prune]"
-        echo ""
-        echo "Arguments:"
-        echo "   install: Initial installation. Configuration file is created in the same directory where the script is launched."
-        echo ""
-        echo "   update: Run update (prune removed branches, update existing and create new branches)"
-        echo ""
+if [ ! -d $local_repository_dir/.git ]; then
+    init_local_repository
 fi
+
+# reset index.html
+
+echo "<html><head><title>git-deploy-branches</title></head><body>" > $indexfile
+
+cd $local_repository_dir
+
+git remote prune origin
+git fetch --all
+
+for branch in `git branch -r | grep -v HEAD | sed -e 's/origin\///'`; do
+        
+    echo "Checking out $branch"
+
+    git checkout $branch
+    git pull
+
+    deploy_branch $branch
+    link_files $branch
+    make_indexfile_row $branch
+done
+
+echo "</body></html>" >> $indexfile
